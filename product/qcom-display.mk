dabe0895ea1f0b86268773542d13723f1e1539cc
BeYkeRYkt <beykerykt@gmail.com>
2017-05-15 22:59:29 +0900

    marmite: add liboverlay to qcom-display

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -3,7 +3,8 @@ PRODUCT_PACKAGES += \
     gralloc.msm8937 \
     copybit.msm8937 \
     hwcomposer.msm8937 \
-    memtrack.msm8937
+    memtrack.msm8937 \
+    liboverlay \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
