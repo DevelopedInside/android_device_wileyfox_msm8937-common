3d9ec2ab38883438cb064799815390cb2fb7d583
BeYkeRYkt <beykerykt@gmail.com>
2017-05-17 19:54:04 +0900

    marmite: remove unsupported media packages

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -5,10 +5,6 @@ PRODUCT_PACKAGES += \
     libOmxCore \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
-    libOmxSwVencMpeg4 \
-    libOmxSwVencHevc \
     libOmxVdec \
-    libOmxVdecHevc \
-    libOmxVidcCommon \
     libOmxVenc \
     libstagefrighthw
